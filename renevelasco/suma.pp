41f81bd63736a3c3082e8e77ed7acbc676825cff
renevelasco <audi-esm@hotmail.com>
2022-03-16 09:27:39 -0500

    realizado desde un celular

--- renevelasco/suma.pp
+++ renevelasco/suma.pp
@@ -1,11 +1,12 @@
-// Rene Velasco - Suma
 #include<iostream>
 using namespace std;
 int main()
 {
-	float A,B,C;
-	cout<<"Ingrese sus cifras: "<<return           cin>return
-	C=A+B;
-	cout<<"El total de la suma es: "<<C<<endl;
-						return 0;
-						}
+float A,B,C;
+cout<<"Ingrese sus cifras: "<<endl;
+cin>>A>>B;
+C=A+B;
+cout<<"El total de la suma es: "<<C<<endl;
+
+return 0;				
+}
