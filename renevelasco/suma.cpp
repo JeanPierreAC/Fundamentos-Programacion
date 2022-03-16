df89cb9302e3106b268b7f79b1f62c5f2bd6692b
renevelasco <audi-esm@hotmail.com>
2022-03-16 09:42:36 -0500

    realizado desde un celular

---
+++ renevelasco/suma.cpp
@@ -0,0 +1,12 @@
+// Rene velasco
+ #include <iostream>
+ using namespace std;
+ int main()
+ {
+	 float A,B,C;
+	 cunt<<"ingrese los valores de la suma:  "<<endl;
+	 cin>>A>>B;
+	 C=A+B;
+	 cunt<<"la respuesta a la suma es: " <<C<<endl;
+	 return 0
+}
