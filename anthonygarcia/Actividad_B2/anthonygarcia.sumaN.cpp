96ada3c251bd1d789cab40e6f3d41afb6444fd7b
anthonymike25 <101486774+anthonymike25@users.noreply.github.com>
2022-05-20 17:18:18 -0500

    Update anthonygarcia.sumaN.cpp

--- anthonygarcia/Actividad_B2/anthonygarcia.sumaN.cpp
+++ anthonygarcia/Actividad_B2/anthonygarcia.sumaN.cpp
@@ -1,25 +1,37 @@
-#include <iostream>
+//================================================
+//==> Nombre del programa: suma de dos numeros
+//==> Archivo : suman.cpp
+//==>Autor: Anthony Garcia
+//==>Fecha de elaboración: 2022-05-02
+//==>Fecha ultima actualización: 2022-05-20
+//=================================================
 
+#include<iostream>
 using namespace std;
 
 int main()
 {
-    int i=0,n;
-    flout x,s=0;
-    cout<<"ingresar n:";
-    cin>>n;
-    do{
-        
-        cout<<"ingresar x:";
-        cin>>x;
-        i=i+1;
-        s=s+x;
-        
-     
-    }while(i<n);
-    
-    cout<<"la cantidad de numeros ingresados es:"<<i<<endl;
-    cout<<"la suma de los numeros fue:"<<s<<endl;
+  int ag_c=0,ag_n;
+  float ag_x,ag_a=0;
+  cout<<"Ingrese la cantidad de número a sumar:"; 
+  cin>>ag_n;
+  do{
+	  cout<<"Ingrese el número : "; 
+	  cin>>ag_x;
+	  ag_c=ag_c+1;
+	  ag_a=ag_a+ag_x;
+  }while(ag_c<ag_n);
 
-    return 0;
-}
+
+cout<<endl<<"//================================================"<<endl;
+cout<<"//==> Nombre del programa: suma de dos numeros"<<endl;
+cout<<"//==> Archivo : suman.cpp"<<endl;
+cout<<"//==>Autor: Anthony Garcia"<<endl;
+cout<<"//==>Fecha de elaboración: 2022-05-02"<<endl;
+cout<<"//==>Fecha ultima actualización: 2022-05-20"<<endl;
+cout<<"//================================================="<<endl;
+
+
+
+  cout<<"La suma de los "<<ag_n<<" números fue "<<ag_a<<endl;
+  return 0;
