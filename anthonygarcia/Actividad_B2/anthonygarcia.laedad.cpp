69b1595f4c52357abb856fddc5e2ad63e908ccc6
anthonymike25 <101486774+anthonymike25@users.noreply.github.com>
2022-05-20 17:59:25 -0500

    Update anthonygarcia.laedad.cpp

--- anthonygarcia/Actividad_B2/anthonygarcia.laedad.cpp
+++ anthonygarcia/Actividad_B2/anthonygarcia.laedad.cpp
@@ -1,20 +1,49 @@
-#include <iostream>
+//================================================
+//==> Nombre del programa: la edad
+//==> Archivo : laedad.cpp
+//==>Autor: Anthony Garcia
+//==>Fecha de elaboración: 2022-05-02
+//==>Fecha ultima actualización: 2022-05-20
+//=================================================
 
+#include<iostream>
 using namespace std;
 
 int main()
 {
-    int edad;
-    
-    cout<<"digite su edad:";
-    cin>>edad;
-    //18-19-20-21-22-23-24
-    if((edad>=18)&&(edad<=24)){
-        cout<<"su edad esta en el rango:";
-    }
-    else{
-        cout<<"su edad esta fuera del rango:";
-    }
+  int ag_aa,ag_ma,ag_da,ag_an,ag_mn,ag_dn,ag_a,ag_m,ag_d;
+	cin>>ag_aa>>ag_ma>>ag_da>>ag_an>>ag_mn>>ag_dn;
+	if(ag_da>=ag_dn){
+		ag_d=ag_da-ag_dn;
+	}
+	else{
+		ag_ma=ag_ma+1;
+		ag_da=ag_da+30;
+		ag_d=ag_da-ag_dn;
+	}
+	if(ag_ma>=ag_mn){
+		ag_m=ag_ma-ag_mn;
+	}
+	else{
+		ag_aa=ag_aa-1;
+		ag_ma=ag_ma+12;
+		ag_m=ag_ma-ag_mn;
+	}
+	ag_a=ag_aa-ag_a;
 
-    return 0;
+cout<<endl<<"//================================================"<<endl;
+cout<<"//==> Nombre del programa: la edad"<<endl;
+cout<<"//==> Archivo : laedad.cpp"<<endl;
+cout<<"//==>Autor: Anthony Garcia"<<endl;
+cout<<"//==>Fecha de elaboración: 2022-05-02"<<endl;
+cout<<"//==>Fecha ultima actualización: 2022-05-20"<<endl;
+cout<<"//================================================="<<endl;
+
+
+    cout<<ag_a<<endl;
+	cout<<ag_m<<endl;
+	cout<<ag_d<<endl;
+    
+  return 0;
+  
 }
