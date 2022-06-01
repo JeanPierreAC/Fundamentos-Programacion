30ffdcae6a01e8aeaf4b62dd5b228d2e0caf4c0b
anthonymike25 <101486774+anthonymike25@users.noreply.github.com>
2022-05-31 21:35:24 -0500

    Update anthonygarcia.laedad.cpp

--- anthonygarcia/Actividad_B2/anthonygarcia.laedad.cpp
+++ anthonygarcia/Actividad_B2/anthonygarcia.laedad.cpp
@@ -12,38 +12,62 @@ using namespace std;
 int main()
 {
   int ag_aa,ag_ma,ag_da,ag_an,ag_mn,ag_dn,ag_a,ag_m,ag_d;
-	cin>>ag_aa>>ag_ma>>ag_da>>ag_an>>ag_mn>>ag_dn;
-	if(ag_da>=ag_dn){
+	cout<<"//Ingrese el año actual: ";
+	cin>>ag_aa;
+	cout<<"//Ingrese el mes actual: ";
+	cin>>ag_ma;
+	cout<<"//Ingrese el día actual: ";
+	cin>>ag_da;
+	cout<<"//Ingrese el año de nacimiento: ";
+	cin>>ag_an;
+	cout<<"//Ingrese el mes de nacimiento: ";
+	cin>>ag_mn;
+	cout<<"//Ingrese el día de nacimiento: ";
+	cin>>ag_dn;
+	if(ag_da>=ag_dn)
+    {
 		ag_d=ag_da-ag_dn;
+    }
+	else
+    {
+		//ag_ma=ag_ma+1;
+		ag_d=ag_da;
 	}
-	else{
-		ag_ma=ag_ma+1;
-		ag_da=ag_da+30;
-		ag_d=ag_da-ag_dn;
-	}
-	if(ag_ma>=ag_mn){
+	if(ag_ma >= ag_mn){
 		ag_m=ag_ma-ag_mn;
 	}
 	else{
+		ag_d=ag_dn-ag_da;
 		ag_aa=ag_aa-1;
-		ag_ma=ag_ma+12;
-		ag_m=ag_ma-ag_mn;
+		ag_m=ag_mn-ag_ma;
 	}
-	ag_a=ag_aa-ag_a;
-
-cout<<endl<<"//================================================"<<endl;
-cout<<"//==> Nombre del programa: la edad"<<endl;
-cout<<"//==> Archivo : laedad.cpp"<<endl;
-cout<<"//==>Autor: Anthony Garcia"<<endl;
-cout<<"//==>Fecha de elaboración: 2022-05-02"<<endl;
-cout<<"//==>Fecha ultima actualización: 2022-05-20"<<endl;
-cout<<"//================================================="<<endl;
+	if(ag_m == 0 )
+        {
+            ag_m=11;
+            ag_a=ag_aa-ag_an-1;
+        }
+	else if (ag_m == 1 && ag_d==ag_da )
+    {
+        ag_m=0;
+        ag_a=ag_aa-ag_an;
+    }
+    else
+    {
+        ag_a=ag_aa-ag_an;
+    }
+    cout<<endl<<"//================================================"<<endl;
+    cout<<"//==> Nombre del programa: la edad"<<endl;
+    cout<<"//==> Archivo : laedad.cpp"<<endl;
+    cout<<"//==>Autor: Anthony Garcia"<<endl;
+    cout<<"//==>Fecha de elaboración: 2022-05-02"<<endl;
+    cout<<"//==>Fecha ultima actualización: 2022-05-20"<<endl;
+    cout<<"//================================================="<<endl;
 
 
     cout<<ag_a<<endl;
 	cout<<ag_m<<endl;
 	cout<<ag_d<<endl;
-    
+
   return 0;
-  
+
 }
